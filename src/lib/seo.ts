4b0b9b75a7fa36c049961644cf8807047dd914a9
Development10 <prashantnadar18@gmail.com>
2026-07-09 13:52:01 +0530

    refactor: remove remaining Lovable runtime dependencies

--- src/lib/seo.ts
+++ src/lib/seo.ts
@@ -1,6 +1,6 @@
 export const SITE_URL = "https://universaltools.in";
 export const SITE_NAME = "UniversalTools";
-export const OG_IMAGE = "https://pub-bb2e103a32db4e198524a2e9ed8f35b4.r2.dev/f3c5fee4-ae85-4a7b-8cdd-ec6cb98cb1d0/id-preview-452bdf43--a3d605dc-67ee-44fc-89bf-14e53a6b13d3.lovable.app-1782811772799.png";
+export const OG_IMAGE = "https://universaltools.in/og-image.png";
 
 export function imageMeta() {
   return [
